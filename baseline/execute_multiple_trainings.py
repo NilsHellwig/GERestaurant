3879c545d9fa33f86ce365e870133bd1a17da92e
Jakob Fehle <JakobFehle@users.noreply.github.com>
2024-04-12 13:23:25 +0200

    Update train script for multi-gpu acceleration

--- baseline/execute_multiple_trainings.py
+++ baseline/execute_multiple_trainings.py
@@ -3,6 +3,6 @@ import subprocess
 
 for task in ["aspect_category", "aspect_category_sentiment", "end_2_end_absa", "target_aspect_sentiment_detection"]:
     for model_type in ["base", "large"]:
-        command = f"python train_baseline.py {task} {model_type}"
+        command = f"accelerate launch --multi_gpu --num_processes=2 train_baseline.py {task} {model_type}"
         process = subprocess.Popen(command, shell=True)
         process.wait()
